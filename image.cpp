d1162b9b67884f8853588fe8b1003eaa0de5e57a
AndersenJ <johnathan.andersen@byu.edu>
2018-01-22 13:01:38 -0700

    Moved font include statements into a separate file

--- image.cpp
+++ image.cpp
@@ -6,60 +6,7 @@
 #include <vector>
 #include <ctime>
 #include "math.h"
-#include "letters.h"
-#include "Adafruit-GFX-Library/Adafruit_GFX.cpp"
-#include "Adafruit-GFX-Library/Fonts/FreeMono12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMono18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMono24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMono9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoBold12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoBold18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoBold24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoBold9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoBoldOblique12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoBoldOblique18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoBoldOblique24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoBoldOblique9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoOblique12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoOblique18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoOblique24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeMonoOblique9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSans12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSans18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSans24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSans9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansBold12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansBold18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansBold24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansBold9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansBoldOblique12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansBoldOblique18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansBoldOblique24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansBoldOblique9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansOblique12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansOblique18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansOblique24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSansOblique9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerif12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerif18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerif24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerif9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifBold12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifBold18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifBold24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifBold9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifBoldItalic12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifBoldItalic18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifBoldItalic24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifBoldItalic9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifItalic12pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifItalic18pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifItalic24pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/FreeSerifItalic9pt7b.h"
-#include "Adafruit-GFX-Library/Fonts/Org_01.h"
-#include "Adafruit-GFX-Library/Fonts/Picopixel.h"
-#include "Adafruit-GFX-Library/Fonts/Tiny3x3a2pt7b"
-#include "Adafruit-GFX-Library/Fonts/TomThumb.h"
+#include "fonts.h"
 #define X_RES0 384
 #define Y_RES0 640
 #define X_RES1 400
@@ -68,8 +15,6 @@
 #define Y_RES2 384
 #define X_RES3 640
 #define Y_RES3 384
-#define ASCII_OFFSET 32
-#define LETTER_HEIGHT 13
 #define DEBUG 2
 
 using namespace std;
@@ -968,10 +913,10 @@ void drawImage3(string roomName, string date, string time, string* reservations,
     }
     
     //draw arrow
-    drawRect((currentBlock-currentBlock%2)*20 - 1, 335, 2, 1, 1);
-    drawRect((currentBlock-currentBlock%2)*20 - 2, 334, 4, 1, 1);
-    drawRect((currentBlock-currentBlock%2)*20 - 3, 333, 6, 1, 1);
-    drawRect((currentBlock-currentBlock%2)*20 - 4, 331, 8, 2, 1);
+    drawRect((currentBlock-currentBlock%2)*20, 335, 2, 1, 1);
+    drawRect((currentBlock-currentBlock%2)*20 - 1, 334, 4, 1, 1);
+    drawRect((currentBlock-currentBlock%2)*20 - 2, 333, 6, 1, 1);
+    drawRect((currentBlock-currentBlock%2)*20 - 3, 331, 8, 2, 1);
 
     //draw time above the arrow
     //canvas->setFont(&FreeSansBold9pt7b);
