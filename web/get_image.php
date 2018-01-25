47b0baaaaa49d5c95a2dd0795b335182804c8f3a
AndersenJ <johnathan.andersen@byu.edu>
2018-01-25 10:21:59 -0700

    Inputs are now sanitized

--- web/get_image.php
+++ web/get_image.php
@@ -1,19 +1,21 @@
 <?php
     $mac_address = $_GET["mac_address"];
     $voltage = $_GET["voltage"];
-    `./get_image.sh $mac_address $voltage 2>&1`;
-    $file = "image_data/" . $mac_address . ".compressed";
-    if (file_exists($file)) {
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.basename($file).'"');
-        header('Expires: 0');
-        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Pragma: no-cache");
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($file));
-        readfile($file);
-        exit;
+    if (preg_match('/^[[:xdigit:]]+$/', $mac_address) === 1 && preg_match('/^[[:digit:].]+$/', $voltage) === 1) {
+        `./get_image.sh $mac_address $voltage 2>&1`;
+        $file = "image_data/" . $mac_address . ".compressed";
+        if (file_exists($file)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.basename($file).'"');
+            header('Expires: 0');
+            header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+            header("Cache-Control: post-check=0, pre-check=0", false);
+            header("Pragma: no-cache");
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($file));
+            readfile($file);
+            exit;
+        }
     }
 ?>
