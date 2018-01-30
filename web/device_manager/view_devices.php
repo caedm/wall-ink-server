d4eb9d0455397c2fdac313668ebe5d5607f2d225
AndersenJ <johnathan.andersen@byu.edu>
2018-01-30 11:19:58 -0700

    Changed variable names to be more clear

--- web/device_manager/view_devices.php
+++ web/device_manager/view_devices.php
@@ -2,16 +2,12 @@
 <?php include 'css/view_devices.css'; ?>
 </style>
 <?php
-function printResult($results) {
+function printResult($devices) {
     //Display how many results there were
-    if ($results->num_rows === 0) {
-        echo "No results<br>";
-        return;
-    }   
     #echo "$res->num_rows entries<br>";
 
     //Display each row
-    $results->data_seek(0);
+    $devices->data_seek(0);
     echo "<table class=\"devices\">";
 
     echo "<tr class=\"headers\">";
@@ -43,36 +39,36 @@ function printResult($results) {
 
     echo "</tr>";
     
-    while ($row = $results->fetch_assoc()) {
-        echo "<tr class=\"device\" onclick=\"document.location = 'edit_device.php?device_id=$row[device_id]'\">";
+    while ($device = $devices->fetch_assoc()) {
+        echo "<tr class=\"device\" onclick=\"document.location = 'edit_device.php?device_id=$device[device_id]'\">";
 
         #echo "<td class=\"device_id\">";
         #echo $row["device_id"];
         #echo "</td>";
         echo "<td class=\"mac_address\">";
-        echo $row["mac_address"];
+        echo $device["mac_address"];
         echo "</td>";
         echo "<td class=\"resource_id\">";
-        echo $row["resource_id"];
+        echo $device["resource_id"];
         echo "</td>";
         echo "<td class=\"device_type\">";
-        echo $row["device_type"];
+        echo $device["device_type"];
         echo "</td>";
         echo "<td class=\"voltage";
-        if ($row["voltage"] < 2.5) {
+        if ($device["voltage"] < 2.5) {
             echo " low-voltage";
         }
         echo "\">";
-        echo $row["voltage"];
+        echo $device["voltage"];
         echo "</td>";
         echo "<td class=\"orientation\">";
-        echo $row["orientation"];
+        echo $device["orientation"];
         echo "</td>";
         echo "<td class=\"last_checked_in\">";
-        echo $row["last_checked_in"];
+        echo $device["last_checked_in"];
         echo "</td>";
         echo "<td class=\"batteries_replaced_date\">";
-        echo $row["batteries_replaced_date"];
+        echo $device["batteries_replaced_date"];
         echo "</td>";
 
         echo "</tr>";
@@ -96,7 +92,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 include 'dbconfig.php';
 $mysqli = mysqli_connect($server, $username, $password, "door-display");
-$results = mysqli_query($mysqli, "SELECT * FROM devices");
-printResult($results);
+$devices = mysqli_query($mysqli, "SELECT * FROM devices");
+printResult($devices);
 ?>
 
