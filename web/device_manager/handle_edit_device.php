e81c4c413cce8d3793728cf1acf61679a026cb48
AndersenJ <johnathan.andersen@byu.edu>
2018-06-21 15:20:27 -0600

    Handle edit device now formats the mac address for you

--- web/device_manager/handle_edit_device.php
+++ web/device_manager/handle_edit_device.php
@@ -4,12 +4,13 @@
     #require_once("print_info.php");
     #printInfo($_POST);
 
+    $mac = strtoupper(str_replace(":","",$_POST["new_mac_address"]));
     include 'dbconfig.php';
     $mysqli = mysqli_connect($server, $username, $password, "door-display");
     if ($_POST["new_device_id"] == "new") {
-        $sql_query="INSERT INTO devices(mac_address,resource_id,orientation,device_type) VALUES (\"$_POST[new_mac_address]\",$_POST[new_resource_id],$_POST[new_orientation],$_POST[new_device_type])";
+        $sql_query="INSERT INTO devices(mac_address,resource_id,orientation,device_type) VALUES (\"$mac\",$_POST[new_resource_id],$_POST[new_orientation],$_POST[new_device_type])";
     } else {
-        $sql_query="UPDATE devices SET mac_address = \"$_POST[new_mac_address]\", resource_id = $_POST[new_resource_id], orientation = $_POST[new_orientation], device_type = $_POST[new_device_type] WHERE device_id = $_POST[new_device_id]";
+        $sql_query="UPDATE devices SET mac_address = \"$mac\", resource_id = $_POST[new_resource_id], orientation = $_POST[new_orientation], device_type = $_POST[new_device_type] WHERE device_id = $_POST[new_device_id]";
     }
     if ($mysqli->query($sql_query) === TRUE) {
         header( "refresh: 3; url=view_devices.php");
