8b8eab38eca1a4e81b7f1adc425e2b2da1a05ce2
AndersenJ <johnathan.andersen@byu.edu>
2018-10-18 11:31:46 -0600

    Fixed get_png to use the new plugin system

--- web/get_png.php
+++ web/get_png.php
@@ -3,24 +3,32 @@
     #require_once("print_info.php");
     #printInfo($_POST);
 
-    #ini_set('display_errors', 1);
-    #ini_set('display_startup_errors', 1);
-    #error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
     $mac_address = $_GET["mac_address"];
-    $png = "image_data/" . $mac_address . ".png";
+    $png = "$_SERVER[DOCUMENT_ROOT]/image_data/" . $mac_address . ".png";
     if (preg_match('/^[[:xdigit:]]+$/', $mac_address) === 1) {
-        include 'config/dbconfig.php';
+        include "$_SERVER[DOCUMENT_ROOT]/config/dbconfig.php";
         $mysqli = mysqli_connect($config->deviceDatabaseServer, $config->deviceDatabaseUsername, $config->deviceDatabasePassword, $config->deviceDatabaseName);
         $result = mysqli_query($mysqli, "SELECT * FROM devices WHERE mac_address = \"$mac_address\"");
         $device = $result->fetch_assoc();
         if ($_GET["layout"] == 5 || $_GET["layout"] == 8) {
-            #$result = mysqli_query($mysqli, $sql_query);
-            $pbm = "image_data/" . $mac_address . ".static.pbm";
+            $pbm = "$_SERVER[DOCUMENT_ROOT]/image_data/" . $mac_address . ".static.pbm";
             `convert $pbm $png`;
         } else {
-            `./get_image.sh $mac_address $device[voltage] 0 $_GET[layout] 2>&1`;
-            $raw = "image_data/" . $mac_address;
+            #`./get_image.sh $mac_address $device[voltage] 0 $_GET[layout] 2>&1`;
+            foreach (glob("$_SERVER[DOCUMENT_ROOT]/plugins/*.php") as $filename) {
+                require_once($filename);
+            }
+            foreach ($plugins as $plugin) {
+                if ($plugin->getIndex() == $device["scheduling_system"]) {
+                    $device["orientation"] = 0;
+                    $compressedFile = $plugin->getImage($config, $mac_address, 3, $device);
+                }
+            }
+            $raw = "$_SERVER[DOCUMENT_ROOT]/image_data/" . $mac_address;
             `./rawToPng.sh $raw $_GET[layout]`;
         }
         if (file_exists($png)) {
