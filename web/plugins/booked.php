764c10671e8dee0ee71b1011eb9e551f75a462db
Daniel Fussell <dfussell@byu.edu>
2018-10-24 11:23:20 -0600

    Get device schedule using one query.

--- web/plugins/booked.php
+++ web/plugins/booked.php
@@ -5,16 +5,21 @@ if ($config->bookedIntegrationActive == "true") {
     class bookedPlugin implements iPlugin {
         public function getIndex() {
             return 0;
-        } 
+        }
         public function getName() {
             return "Booked";
-        } 
+        }
         public function isActive($config) {
             return $config->bookedIntegrationActive;
         }
         public function getResources($config) {
             $rooms = array();
-            $mysqli = mysqli_connect($config->bookedDatabaseServer, $config->bookedDatabaseUsername, $config->bookedDatabasePassword, $config->bookedDatabaseName);
+            $mysqli = mysqli_connect(
+                $config->bookedDatabaseServer,
+                $config->bookedDatabaseUsername,
+                $config->bookedDatabasePassword,
+                $config->bookedDatabaseName
+            );
             $resources = mysqli_query($mysqli, "SELECT resource_id,name FROM resources");
             while($room = $resources->fetch_assoc()){
                 $rooms[ $room["resource_id"] ] = $room["name"];
@@ -22,33 +27,54 @@ if ($config->bookedIntegrationActive == "true") {
             return $rooms;
         }
         public function getSchedule($config, $resourceId) {
-            $mysqli = mysqli_connect($config->bookedDatabaseServer, $config->bookedDatabaseUsername, $config->bookedDatabasePassword, $config->bookedDatabaseName);
+            $mysqli = mysqli_connect(
+                $config->bookedDatabaseServer,
+                $config->bookedDatabaseUsername,
+                $config->bookedDatabasePassword,
+                $config->bookedDatabaseName
+            );
             $name = mysqli_query($mysqli, "SELECT name FROM resources WHERE resource_id = $resourceId");
             $schedule = "";
             while($row = $name->fetch_assoc()){
                 $schedule = $row['name'];
             }
             $schedule .= "\n";
-            $seriesIds = mysqli_query($mysqli, "SELECT series_id FROM reservation_resources WHERE resource_id = $resourceId AND series_id NOT IN (SELECT series_id FROM reservation_series WHERE status_id = 2)");
-            while($row = $seriesIds->fetch_assoc()){
-                $reservationInstanceIds = mysqli_query($mysqli, "SELECT reservation_instance_id FROM reservation_instances WHERE series_id = $row[series_id] AND DATE(CONVERT_TZ(start_date,'+0:00','SYSTEM')) <= DATE(NOW()) AND DATE(CONVERT_TZ(end_date,'+0:00','SYSTEM')) >= DATE(NOW())");
-                while ($resInstanceId = $reservationInstanceIds->fetch_assoc()) {
-                    $reservationsInfo = mysqli_query($mysqli, "SELECT * FROM (SELECT CONVERT_TZ(start_date,'+00:00','SYSTEM'),CONVERT_TZ(end_date,'+00:00','SYSTEM') FROM reservation_instances WHERE reservation_instance_id = $resInstanceId[reservation_instance_id]) AS A JOIN(SELECT title FROM reservation_series WHERE series_id = $row[series_id]) AS B");
-                    while ($reservationInfo = $reservationsInfo->fetch_assoc()) {
-                        $schedule .= $reservationInfo['title'];
-                        $schedule .= "\n";
-                        $schedule .= $reservationInfo["CONVERT_TZ(start_date,'+00:00','SYSTEM')"];
-                        $schedule .= "\n";
-                        $schedule .= $reservationInfo["CONVERT_TZ(end_date,'+00:00','SYSTEM')"];
-                        $schedule .= "\n";
-                    }
-                }
+            $query = "SELECT
+                        name,
+                        title,
+                        CONVERT_TZ(start_date, '+0:00', 'SYSTEM') AS start_date,
+                        CONVERT_TZ(end_date,   '+0:00', 'SYSTEM') AS end_date
+                      FROM
+                        resources t1
+                        JOIN reservation_resources t2 ON (t1.resource_id = t2.resource_id)
+                        JOIN reservation_series    t3 ON (t2.series_id = t3.series_id)
+                        JOIN reservation_instances t4 ON (t3.series_id = t4.series_id)
+                      WHERE
+                        t1.resource_id = $resourceId
+                        AND DATE(CONVERT_TZ(start_date, '+0:00', 'SYSTEM')) <= CURRENT_DATE()
+                        AND DATE(CONVERT_TZ(end_date,   '+0:00', 'SYSTEM')) >= CURRENT_DATE()
+                        AND t3.status_id != 2
+                      ORDER BY start_date
+                     ";
+
+            $reservations = mysqli_query($mysqli, $query);
+            while ($reservation = $reservations->fetch_assoc()) {
+                $schedule .= $reservation['title']      . "\n"
+                           . $reservation['start_date'] . "\n"
+                           . $reservation['end_date']   . "\n";
             }
+
             return $schedule;
         }
         public function getImage($config, $device) {
             require("$_SERVER[DOCUMENT_ROOT]/plugin_dependencies/general_scheduling/schedulingGetImage.php");
-            return schedulingGetImage($config, $device, $this->getSchedule($config, $device["resource_id"]));
+            return schedulingGetImage(
+                $config,
+                $device,
+                $this->getSchedule($config, $device["resource_id"]),
+                $config->bookedDisplayUrl,
+                $config->bookedQrCodeBaseUrlBeginning,
+                $config->bookedBaseUrlEnd);
         }
         public function getDeviceType($device) {
             require("$_SERVER[DOCUMENT_ROOT]/plugin_dependencies/general_scheduling/schedulingGetDeviceType.php");
