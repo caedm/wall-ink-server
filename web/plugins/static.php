7a7db1f593e6e3ab157389ed690333bb3453a488
AndersenJ <johnathan.andersen@byu.edu>
2018-11-06 10:35:25 -0700

    Finished the static plugin, at least for now

--- web/plugins/static.php
+++ web/plugins/static.php
@@ -41,26 +41,102 @@ if ($config->staticImagesPluginActive == "true") {
             $imagesDir = $config->runTimeWebDirectory . "/plugin_dependencies/static_images/" . $device["resource_id"] . '/*';
             $images = glob($imagesDir);
 
-            #$sourceImage = "";
-            #foreach ($images as $image) {
-                #$sourceImage = $image;
-            #}
-            $timeIncrement = 300;
+            $timeIncrement = 3600;
+            if ($device['device_type'] == 1 || $device['device_type'] == 2) {
+                $timeIncrement = 1800;
+            } else if ($device['device_type'] == 4 || $device['device_type'] == 3) {
+                $timeIncrement = 3600;
+            } else if ($device['device_type'] == 6 || $device['device_type'] == 7) {
+                $timeIncrement = 10800;
+            } else if ($device['device_type'] == 9 || $device['device_type'] == 10) {
+                $timeIncrement = 86400;
+            }
             $nextRefreshTime = $timeIncrement - ($_SERVER['REQUEST_TIME'] % $timeIncrement);
-            #$images = array_values($images);
-            #sort($images);
             $sourceImage = $images[floor($_SERVER['REQUEST_TIME'] / $timeIncrement) % count($images)];
             $pbm = "$_SERVER[DOCUMENT_ROOT]/image_data/" . $device["mac_address"] . "." . "pbm";
             $raw = "$_SERVER[DOCUMENT_ROOT]/image_data/" . $device["mac_address"];
             $static = "$_SERVER[DOCUMENT_ROOT]/image_data/" . $device["mac_address"] . "." . "static";
+            $angle = 0;
+            if ($device['orientation'] == 1) {
+                $angle = 180;
+            }
             `convert $sourceImage -rotate 180 -negate -resize $size\! $pbm`;
             `$_SERVER[DOCUMENT_ROOT]/pbmToRaw.sh $pbm $raw`;
             `$_SERVER[DOCUMENT_ROOT]/rawToCompressed $raw $static $width $height $nextRefreshTime`;
             return $static;
         }
         public function getDeviceType($device) {
-            require("$_SERVER[DOCUMENT_ROOT]/plugin_dependencies/general_scheduling/schedulingGetDeviceType.php");
-            return schedulingGetDeviceType($device);
+            $getDeviceType = "";
+            $getDeviceType .= "<fieldset class=\"field\">";
+                $getDeviceType .= "<legend>Device Type</legend>";
+                $getDeviceType .= "<ul>";
+                    $getDeviceType .= "<li>";
+                        $getDeviceType .= "<label for=\"1\">4\" static, 30 minute refresh cycle</label>";
+                        $getDeviceType .= "<input type=\"radio\" id=\"type_1\" name=\"new_device_type\" value=\"1\"";
+                        if ($device['device_type'] == 1) {
+                            $getDeviceType .= " checked";
+                        }
+                        $getDeviceType .= ">";
+                    $getDeviceType .= "</li>";
+                    $getDeviceType .= "<li>";
+                        $getDeviceType .= "<label for=\"4\">4\" static, 1 hour refresh cycle</label>";
+                        $getDeviceType .= "<input type=\"radio\" id=\"type_4\" name=\"new_device_type\" value=\"4\"";
+                        if ($device['device_type'] == 4) {
+                            $getDeviceType .= " checked";
+                        }
+                        $getDeviceType .= ">";
+                    $getDeviceType .= "</li>";
+                    $getDeviceType .= "<li>";
+                        $getDeviceType .= "<label for=\"6\">4\" static, 3 hour refresh cycle</label>";
+                        $getDeviceType .= "<input type=\"radio\" id=\"type_6\" name=\"new_device_type\" value=\"6\"";
+                        if ($device['device_type'] == 6) {
+                            $getDeviceType .= " checked";
+                        }
+                        $getDeviceType .= ">";
+                    $getDeviceType .= "</li>";
+                    $getDeviceType .= "<li>";
+                        $getDeviceType .= "<label for=\"9\">4\" static, 1 day refresh cycle</label>";
+                        $getDeviceType .= "<input type=\"radio\" id=\"type_9\" name=\"new_device_type\" value=\"9\"";
+                        if ($device['device_type'] == 9) {
+                            $getDeviceType .= " checked";
+                        }
+                        $getDeviceType .= ">";
+                    $getDeviceType .= "</li>";
+                    $getDeviceType .= "<li>";
+                        $getDeviceType .= "<label for=\"2\">7\" static, 30 minute refresh cycle</label>";
+                        $getDeviceType .= "<input type=\"radio\" id=\"type_2\" name=\"new_device_type\" value=\"2\"";
+                        if ($device['device_type'] == 2) {
+                            $getDeviceType .= " checked";
+                        }
+                        $getDeviceType .= ">";
+                    $getDeviceType .= "</li>";
+                    $getDeviceType .= "<li>";
+                        $getDeviceType .= "<label for=\"3\">7\" static, 1 hour refresh cycle</label>";
+                        $getDeviceType .= "<input type=\"radio\" id=\"type_3\" name=\"new_device_type\" value=\"3\"";
+                        if ($device['device_type'] == 3) {
+                            $getDeviceType .= " checked";
+                        }
+                        $getDeviceType .= ">";
+                    $getDeviceType .= "</li>";
+                    $getDeviceType .= "<li>";
+                        $getDeviceType .= "<label for=\"7\">7\" static, 3 hour refresh cycle</label>";
+                        $getDeviceType .= "<input type=\"radio\" id=\"type_7\" name=\"new_device_type\" value=\"7\"";
+                        if ($device['device_type'] == 7) {
+                            $getDeviceType .= " checked";
+                        }
+                        $getDeviceType .= ">";
+                    $getDeviceType .= "</li>";
+                    $getDeviceType .= "<li>";
+                        $getDeviceType .= "<label for=\"10\">7\" static, 1 day refresh cycle</label>";
+                        $getDeviceType .= "<input type=\"radio\" id=\"type_10\" name=\"new_device_type\" value=\"10\"";
+                        if ($device['device_type'] == 10) {
+                            $getDeviceType .= " checked";
+                        }
+                        $getDeviceType .= ">";
+                    $getDeviceType .= "</li>";
+                $getDeviceType .= "</ul>";
+            $getDeviceType .= "</fieldset>";
+            return $getDeviceType;
         }
     }
     $staticImages = new staticImagesPlugin;
