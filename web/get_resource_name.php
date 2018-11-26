e370677468faf02b63bdd66083e921f175706565
AndersenJ <johnathan.andersen@byu.edu>
2018-11-26 14:29:11 -0700

    Added a simple API to get a resource name based off of a plugin id and resource id

---
+++ web/get_resource_name.php
@@ -0,0 +1,29 @@
+<?php
+    require('config/dbconfig.php');
+    #Debug stuff
+    if ($config->debugModeActive == "true") {
+        error_reporting(E_ALL);
+        ini_set('display_errors', '1');
+    }
+
+    $plugin_id = $_GET["plugin_id"];
+    $resource_id = $_GET["resource_id"];
+
+    //Sanity check on incoming url 
+    if (preg_match('/^[[:xdigit:]]+$/', $plugin_id) === 1) {
+        $resource_name = "";
+        foreach (glob("$_SERVER[DOCUMENT_ROOT]/plugins/*.php") as $filename) {
+            require_once($filename);
+        }
+        foreach ($plugins as $plugin) {
+            if ($plugin->getIndex() == $plugin_id) {
+                $resources = $plugin->getResources($config);
+                $resource = array();
+                $resource['resource_name'] = $resources[$resource_id];
+                echo json_encode($resource);
+            }
+        }
+    } else {
+        echo "REGEX doesn't match";
+    }
+?>
