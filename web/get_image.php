892b0915482c7d33b38a937e92087e6b7ac3dafc
AndersenJ <johnathan.andersen@byu.edu>
2018-11-28 10:54:04 -0700

    The process of sending an error to a screen has been abstracted out into its own function for later use

--- web/get_image.php
+++ web/get_image.php
@@ -13,6 +13,46 @@ $errorCode = $_GET["error"];
 $width = $_GET["width"];
 $height = $_GET["height"];
 
+function returnError($mac_address, $voltage, $firmware_version, $width, $height, $error) {
+    //Since there is no database entry for this device
+    //Set to use the simple text plugin
+    //Get as much information from the device query as possible to 
+    //pass into the the plugin 
+    $device["resource_id"] = "error";
+    $device["device_type"] = "30";
+    $device["mac_address"] = $mac_address;
+    $device["voltage"] = $voltage;
+    $device["orientation"] = "1";
+    $device["firmware_version"] = $firmware_version;
+    $device["scheduling_system"] = "4";
+    $device["height"] = $height;
+    $device["width"] = $width;
+    $device["error_message"] = $error;	
+
+    //use the simple text plugin to display unknown screen size message
+    require_once("./plugins/simple_text.php");
+
+    foreach ($plugins as $plugin) {
+        if ($plugin->getIndex() == $device["scheduling_system"]) {
+            $file = $plugin->getImage($config, $device);
+        }
+    }
+
+    if (file_exists($file)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename="'.basename($file).'"');
+        header('Expires: 0');
+        header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+        header('Cache-Control: post-check=0, pre-check=0', false);
+        header('Pragma: no-cache');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($file));
+        readfile($file);
+        exit;
+    }
+}
+
 //Sanity check on incoming url 
 if (preg_match('/^[[:xdigit:]]{1,50}$/', $mac_address) === 1 && preg_match('/^[[:digit:].]{1,50}$/', $voltage) === 1 && preg_match('/^[[:alphanum:]._]{1,50}$/', $firmware_version) === 1 && preg_match('/^[[:digit:]]{1,50}$/', $errorCode) === 1 && preg_match('/^[[:digit:]]{1,50}$/', $width) === 1 && preg_match('/^[[:digit:]]{1,50}$/', $height) === 1) {
 
@@ -67,47 +107,9 @@ if (preg_match('/^[[:xdigit:]]{1,50}$/', $mac_address) === 1 && preg_match('/^[[
             exit;
         }
     } else {
-        //MAC address is unknown:
-        //Give the unknown mac address a file with mac and instructions:
-
-        //Since there is no database entry for this device
-        //Set to use the simple text plugin
-        //Get as much information from the device query as possible to 
-        //pass into the the plugin 
-        $device["resource_id"] = "error";
-        $device["device_type"] = "30";
-        $device["mac_address"] = $mac_address;
-        $device["voltage"] = $voltage;
-        $device["orientation"] = "1";
-        $device["firmware_version"] = $firmware_version;
-        $device["scheduling_system"] = "4";
-        $device["height"] = $height;
-        $device["width"] = $width;
-        $device["error_message"] = "Unknown MAC address";	
-
-        //use the simple text plugin to display unknown screen size message
-        require_once("./plugins/simple_text.php");
-
-        foreach ($plugins as $plugin) {
-            if ($plugin->getIndex() == $device["scheduling_system"]) {
-                $file = $plugin->getImage($config, $device);
-            }
-        }
-
-        if (file_exists($file)) {
-            header('Content-Description: File Transfer');
-            header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename="'.basename($file).'"');
-            header('Expires: 0');
-            header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
-            header('Cache-Control: post-check=0, pre-check=0', false);
-            header('Pragma: no-cache');
-            header('Pragma: public');
-            header('Content-Length: ' . filesize($file));
-            readfile($file);
-            exit;
-        }
-    } //end of unknown mac
+        // this code block gets executed if the mac address doesn't match an existing device in the device database
+        returnError($mac_address, $voltage, $firmware_version, $width, $height, "Unknown MAC address");
+    }
 } else {
     // if URL does not pass sanity check:
     echo "REGEX doesn't match";
